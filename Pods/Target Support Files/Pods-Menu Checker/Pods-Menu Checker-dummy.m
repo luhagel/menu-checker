067958ae75f13738a7c85ca1e34510990c769a3f
Luca Hagel <hagel.luca@gmail.com>
2016-07-11 10:21:31 -0700

    Init and Install Pod
    ignore xcuserdata

---
+++ Pods/Target Support Files/Pods-Menu Checker/Pods-Menu Checker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Menu_Checker : NSObject
+@end
+@implementation PodsDummy_Pods_Menu_Checker
+@end
---
+++ Pods/Target Support Files/Pods-Menu Checker/Pods-Menu Checker-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Menu_CheckerVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Menu_CheckerVersionString[];
+
